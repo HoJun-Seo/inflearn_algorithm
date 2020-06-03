ad93e5e65815d6d50503c527da08f47a3b72d369
SeoHoJun <ghwns6659@naver.com>
2020-06-03 13:49:22 +0900

    algorithm

--- Sorting/src/Sorting/Main.java
+++ Sorting/src/Sorting/Main.java
@@ -8,42 +8,53 @@ import java.util.StringTokenizer;
 public class Main{
 	public static void main(String []args) throws IOException{
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    String str1 = "";
-	    StringTokenizer st1 = null;
+	    String str = "";
+	    StringTokenizer st = null;
 	    
-	    int [][]array = new int[20][20];
-	    int index1, index2;
-	    
-	    for(int i = 1; i <= 19; i++) {
-	    	str1 = br.readLine();
-    		st1 = new StringTokenizer(str1);
-	    	for(int j = 1; j <= 19; j++) {
-	    		array[i][j] = Integer.parseInt(st1.nextToken());
+	    int [][]maze = new int[10][10];
+	    int data = 0;
+	    for(int i = 1; i <= 10; i++) {
+	    	str = br.readLine();
+	    	st = new StringTokenizer(str);
+	    	for(int j = 1; j <= 10; j++) {
+	    		data = Integer.parseInt(st.nextToken());
+	    		maze[i - 1][j - 1] = data;
 	    	}
 	    }
 	    
-	    str1 = br.readLine();
-	    st1 = new StringTokenizer(str1);
-	    int count = Integer.parseInt(st1.nextToken());
-	    
-	    for(int i = 1; i <= count; i++) {
-	    	str1 = br.readLine();
-	    	st1 = new StringTokenizer(str1);
-	    	index1 = Integer.parseInt(st1.nextToken());
-	    	index2 = Integer.parseInt(st1.nextToken());
-	    	for(int j = 1; j <= 19; j++) {
-	    		if(array[index1][j] == 0) array[index1][j] = 1;
-	    		else array[index1][j] = 0;
+	    int index1 = 2; int index2 = 2;
+	    while(true) {
+	    	
+	    	if(maze[index1 - 1][index2 - 1] == 2) {
+	    		maze[index1 - 1][index2 - 1] = 9;
+	    		break;
 	    	}
-	    	for(int j = 1; j <= 19; j++) {
-	    		if(array[j][index2] == 0) array[j][index2] = 1;
-	    		else array[j][index2] = 0;
+	    	else maze[index1 - 1][index2 - 1] = 9;
+	    	
+	    	if(maze[index1 - 1][index2 - 1 + 1] == 1) {
+	    		if(maze[index1 - 1 + 1][index2 - 1] == 1) break;
+	    		else {
+	    			index1++;
+	    			if(maze[index1 - 1][index2 - 1] == 2) {
+	    				maze[index1 - 1][index2 - 1] = 9;
+	    				break;
+	    			}
+	    			maze[index1 - 1][index2 - 1] = 9;
+	    		}
+	    	}
+	    	else {
+	    		index2++;
+	    		if(maze[index1 - 1][index2 - 1] == 2) {
+	    			maze[index1 - 1][index2 - 1] = 9;
+	    			break;
+	    		}
+	    		maze[index1 - 1][index2 - 1] = 9;
 	    	}
 	    }
 	    
-	    for(int i = 1; i <= 19; i++) {
-	    	for(int j = 1; j <= 19; j++) {
-	    		System.out.printf("%d ", array[i][j]);
+	    for(int i = 1; i <= 10; i++) {
+	    	for(int j = 1; j <= 10; j++) {
+	    		System.out.printf("%d ", maze[i - 1][j - 1]);
 	    	}
 	    	System.out.println();
 	    }
